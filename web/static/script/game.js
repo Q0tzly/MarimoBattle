b41422977f8f6dfa5bfb9f2ca12f1b3afa305d45
Q0tzly <g9164902@gmail.com>
2024-05-18 13:33:07 +0900

    test: 動かないけど、とりあえずpush

--- web/static/script/game.js
+++ web/static/script/game.js
@@ -3,15 +3,10 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-var mx = 0;
-var my = 0;
+var center.x = canvas.width / 2;
+var center.y = canvas.height / 2;
 
-var wx = window.innerWidth;
-var wy = window.innerHeight;
-
-var x = 30;
-var y = 30;
-
+/*
 document.addEventListener('onmousemove', onmousemove);
 onmousemove = function(e) {
   mx = e.pageX; my = e.pageY;
@@ -19,28 +14,18 @@ onmousemove = function(e) {
 
 document.addEventListener('onmouseout', onmouseout);
 onmouseout = function(e) {
-  mx = wx/2; my = wy;
+  mx = center.x; my = center.y;
 }
+*/
 
 window.addEventListener("load", update);
 
 function update() {  
-  ctx.clearRect(0, 0, 9999, 9999);
-
-  if (mx > wx/2) {
-    x += 2;
-  } else if (mx < wx/2) {
-    x -= 2;
-  }
-  if (my > wy/2) {
-    y += 2;
-  } else if (my < wy/2) {
-    y -=2;
-  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   var image = new Image();
   image.src = "/img/marimo1.png";
-  ctx.drawImage(image, x, y, 30, 30);
+  ctx.drawImage(image, center.x, center.y, imageWidth, imageHeight);
 
   window.requestAnimationFrame(update);
 }
